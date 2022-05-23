5da43d701de323a199787f6c7265270d0ad1ef8d
Alexey Okhotnikov <deathpressence@gmail.com>
2022-05-23 11:12:47 +0300

    update

---
+++ _nuxt/static/1653292488/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffanbaza-landing\u002F_nuxt\u002Fstatic\u002F1653292488",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffanbaza-landing\u002F",assetsPath:"\u002Ffanbaza-landing\u002F_nuxt\u002F",cdnURL:a}}}}(null));
